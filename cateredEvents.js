7c560e943c4c2fdc8c5f920941d41169ea0bdce8
Ahmer Malik <ahmer9800@gmail.com>
2019-01-10 18:37:02 -0600

    add catering html and events calculator algorithm

--- cateredEvents.js
+++ cateredEvents.js
@@ -1,23 +1,20 @@
 //find the amount of catered events based on today's date.
 //shish kabob does about 6 catered event per month
 
-
-
-
-
 let todayDate = new Date();
 let month = todayDate.getMonth();
 
 
 getEventsDone = (arr, month) => {
-
-
     const eventsDoneLastTwentyFourYears = 1440;
-    return[arr, month];
+    let totalEventsThisYear = 0;
 
+    for (let i = 0; i < month+1; i++){
+      totalEventsThisYear += arr[i];
+      
+    }
+    return [totalEventsThisYear + eventsDoneLastTwentyFourYears, totalEventsThisYear];
 }
-
-
-console.log(getEventsDone([4, 5, 4, 7, 4, 8, 6, 5, 4, 7, 3, 5], month));
+console.log(getEventsDone([6, 5, 4, 7, 4, 8, 6, 5, 4, 7, 3, 5], 11));
 
 //events as of 12/30/2018
